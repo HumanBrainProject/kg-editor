4ea92780c70782d26d78736a79718a466db51d2d
gdenerva <gilles.denervaud@epfl.ch>
2018-12-07 22:05:42 +0100

    split datasets statistics api

--- src/Stores/StatisticsStore.js
+++ src/Stores/StatisticsStore.js
@@ -4,26 +4,55 @@ import API from "../Services/API";
 class StatisticsStore{
 
   @observable statistics = {};
-  @observable isFetched = false;
-  @observable isFetching = false;
-  @observable fetchError = null;
+  @observable globalDatasetsStatistics = {
+    data: {},
+    isFetched: false,
+    isFetching: false,
+    fetchError: null
+  };
+  @observable perWeekDatasetsStatistics = {
+    data: [],
+    isFetched: false,
+    isFetching: false,
+    fetchError: null
+  };
 
   @action
-  async fetchStatistics() {
+  async fetchGlobalDatasetsStatistics() {
     try {
-      this.isFetching = true;
-      this.fetchError = null;
-      const { data } = await API.axios.get(API.endpoints.statistics());
+      this.globalDatasetsStatistics.isFetching = true;
+      this.globalDatasetsStatistics.fetchError = null;
+      const { data } = await API.axios.get(API.endpoints.globalDatasetsStatistics());
       runInAction(() => {
-        this.isFetched = true;
-        this.isFetching = false;
-        this.statistics = data;
+        this.globalDatasetsStatistics.isFetched = true;
+        this.globalDatasetsStatistics.isFetching = false;
+        this.globalDatasetsStatistics.data = (data && data.data)?data.data:{};
       });
     } catch (e) {
       runInAction(() => {
         const message = e.message? e.message: e;
-        this.fetchError = `Error while retrieving list of features (${message})`;
-        this.isFetching = false;
+        this.globalDatasetsStatistics.fetchError = `Error while retrieving datasets statistics (${message})`;
+        this.globalDatasetsStatistics.isFetching = false;
+      });
+    }
+  }
+
+  @action
+  async fetchPerWeekDatasetsStatistics() {
+    try {
+      this.perWeekDatasetsStatistics.isFetching = true;
+      this.perWeekDatasetsStatistics.fetchError = null;
+      const { data } = await API.axios.get(API.endpoints.perWeekDatasetsStatistics());
+      runInAction(() => {
+        this.perWeekDatasetsStatistics.isFetched = true;
+        this.perWeekDatasetsStatistics.isFetching = false;
+        this.perWeekDatasetsStatistics.data = (data && Array.isArray(data.data))?data.data:[];
+      });
+    } catch (e) {
+      runInAction(() => {
+        const message = e.message? e.message: e;
+        this.perWeekDatasetsStatistics.fetchError = `Error while retrieving per week datasets statistics (${message})`;
+        this.perWeekDatasetsStatistics.isFetching = false;
       });
     }
   }

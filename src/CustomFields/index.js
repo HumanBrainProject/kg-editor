4ee339a51dc12e0b168775452c1f8bd0f7d64497
gdenerva <gilles.denervaud@epfl.ch>
2019-02-14 11:12:10 +0100

    integrate alternatives in dropdownSelect

--- src/CustomFields/index.js
+++ src/CustomFields/index.js
@@ -2,9 +2,11 @@ import { FormStore } from "hbp-quickfire";
 
 import KgInputTextField from "./KgInputTextField";
 import KgTextAreaField from "./KgTextAreaField";
+import KgDropdownSelectField from "./KgDropdownSelectField";
 
 FormStore.registerCustomField("KgInputText", KgInputTextField, FormStore.typesMapping.InputText);
 FormStore.registerCustomField("KgTextArea", KgTextAreaField, FormStore.typesMapping.TextArea);
+FormStore.registerCustomField("KgDropdownSelect", KgDropdownSelectField, FormStore.typesMapping.DropdownSelect);
 
 export default {
   KgInputTextField:{
@@ -14,5 +16,9 @@ export default {
   KgTextAreaField:{
     component:KgTextAreaField,
     store:FormStore.typesMapping.TextArea
+  },
+  KgDropdownSelectField:{
+    component:KgDropdownSelectField,
+    store:FormStore.typesMapping.DropdownSelect
   }
 };
--- src/Stores/InstanceStore.js
+++ src/Stores/InstanceStore.js
@@ -392,41 +392,76 @@ class InstanceStore {
         instanceData.alternatives["http://schema.org/name"] = [
           {
             value: "Alternative 2",
-            users: [
-              "12345"
-            ]
+            userIds: "12345"
           },
           {
             value: "Alternative 3",
-            users: [
-              "2468", "9876"
-            ]
+            userIds: ["2468", "9876"]
           },
           {
             value: "Alternative 4",
-            users: [
-              "8642"
-            ]
+            userIds: "8642"
           }
         ];
         instanceData.alternatives["http://schema.org/description"] = [
           {
             value: "This is an second alternative description.",
-            users: [
-              "8642", "9876"
-            ]
+            userIds: ["8642", "9876"]
           },
           {
             value: "This is an third alternative description.",
-            users: [
-              "2468"
-            ]
+            userIds: "2468"
           },
           {
             value: "This is an fourth alternative description.",
-            users: [
-              "12345"
-            ]
+            userIds: "12345"
+          }
+        ];
+        instanceData.alternatives["http://schema.org/description"] = [
+          {
+            value: "This is an second alternative description.",
+            userIds: ["8642", "9876"]
+          },
+          {
+            value: "This is an third alternative description.",
+            userIds: "2468"
+          },
+          {
+            value: "This is an fourth alternative description.",
+            userIds: "12345"
+          }
+        ];
+        instanceData.alternatives["https://schema.hbp.eu/minds/contributors"] = [
+          {
+            value: [
+              {
+                id: "minds/core/person/v1.0.0/36d56617-e253-4b9c-94cc-f74a869c2411"
+              },
+              {
+                id: "minds/core/person/v1.0.0/949aa9a5-ae01-4de3-847a-74b65543a2e3"
+              },
+              {
+                id: "minds/core/person/v1.0.0/a79d7b48-8a57-433c-a9d6-50372bbc9ad2"
+              },
+              {
+                id: "minds/core/person/v1.0.0/4283f0b4-2fef-4a80-a9de-bd2d512161cb"
+              }
+            ],
+            userIds: "12345"
+          },
+          {
+            value: [
+              {
+                id: "minds/core/person/v1.0.0/4283f0b4-2fef-4a80-a9de-bd2d512161cb"
+              },
+              {
+                id: "minds/core/person/v1.0.0/36d56617-e253-4b9c-94cc-f74a869c2411"
+              },
+              {
+                id: "minds/core/person/v1.0.0/949aa9a5-ae01-4de3-847a-74b65543a2e3"
+              }
+            ],
+            userIds: ["2468", "8642"]
           }
         ];
         for(let fieldKey in instanceData.fields){
@@ -435,6 +470,8 @@ class InstanceStore {
             field.type = "KgInputText";
           } else if(field.type === "TextArea"){
             field.type = "KgTextArea";
+          } else if(field.type === "DropdownSelect"){
+            field.type = "KgDropdownSelect";
           }
         }
         instance.form = new FormStore(instanceData);

94ac534bee726f4a6d0b7939d7a55ebc994db90e
gdenerva <gilles.denervaud@epfl.ch>
2019-06-12 10:02:12 +0200

    inline checkbox with its label

--- src/Views/Instance/InstanceForm/InstanceField.js
+++ src/Views/Instance/InstanceForm/InstanceField.js
@@ -29,6 +29,26 @@ const styles = {
     },
     "& .quickfire-field-disabled.quickfire-empty-field, .quickfire-field-readonly.quickfire-empty-field": {
       display: "none"
+    },
+    "& .quickfire-field-checkbox .quickfire-label": {
+      "&:after": {
+        display: "none"
+      },
+      "& + .checkbox": {
+        display: "inline-block",
+        margin: "0 0 0 4px",
+        verticalAlign: "middle",
+        "& label input[type=checkbox]": {
+          fontSize: "16px"
+        }
+      },
+      "& + span": {
+        verticalAlign: "text-bottom",
+        "& input[type=checkbox]": {
+          fontSize: "16px",
+          marginTop: "0"
+        }
+      }
     }
   }
 };

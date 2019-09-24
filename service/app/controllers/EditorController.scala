d2a86c43a1c0f78f17f25ec88d5e125f09bca4f0
Ioannis Tsanaktsidis <tsanakts.10@gmail.com>
2019-09-24 11:56:05 +0200

    refactors all fields true
    Signed-off-by: Ioannis Tsanaktsidis <tsanakts.10@gmail.com>
    Co-authored-by: Gilles Dénervaud <gilles.denervaud@epfl.ch>

--- service/app/controllers/EditorController.scala
+++ service/app/controllers/EditorController.scala
@@ -224,101 +224,182 @@ class EditorController @Inject()(
   def normalizeIdOfArray(fieldArray: List[Map[String, JsValue]]): List[Map[String, JsValue]] =
     fieldArray.map(field => normalizeIdOfField(field))
 
-  def normalizeFieldValue(value: JsValue): JsValue =
-    value.asOpt[JsArray] match {
-      case Some(valueArray) => Json.toJson(normalizeIdOfArray(valueArray.as[List[Map[String, JsValue]]]))
-      case None =>
-        value.asOpt[JsObject] match {
-          case Some(valueObj) => Json.toJson(normalizeIdOfField(valueObj.as[Map[String, JsValue]]))
-          case None           => value
-        }
-    }
-
-  def normalizeField(field: Map[String, JsValue], info: Map[String, JsValue]): Map[String, JsValue] = {
-    val res = Map[String, JsValue]()
-    //    if(info.nonEmpty) {
-    val res1 = info.get("https://schema.hbp.eu/client/kg-editor/widgetType") match {
-      case Some(typeInfo) => res.updated("type", typeInfo)
-      case None           => res
-    }
-    //    val res2 = info.get("https://schema.hbp.eu/client/kg-editor/widgetType") match {
-    //      case Some(markdown) =>
-    //        markdown.as[Boolean] match {
-    //          case true  => res1.updated("markdown", JsString(true.toString))
-    //          case false => res1.updated("markdown", JsString(false.toString))
-    //        }
-    //      case None => res1
-    //    }
-    //    }
-    val res3 = field.get("https://schema.hbp.eu/value") match {
-      case Some(value) =>
-        field.get("https://schema.hbp.eu/isLink") match {
-          case Some(link) =>
-            if (link.as[Boolean]) {
-              res1.updated("value", normalizeFieldValue(value))
-            } else {
-              res1.updated("value", value)
-            }
-          case None => res1.updated("value", value)
-        }
-      case None => res1
-    }
-    field.get("https://schema.hbp.eu/label") match {
-      case Some(value) => res3.updated("label", value)
-      case None        => res3
-    }
-  }
-
-  def normalizeInstance(instance: Map[String, JsValue], fieldsInfo: Map[String, JsValue]): Map[String, JsValue] = {
-
-    val normalizedInstance = List[(String, String, Option[Function1[String, Option[String]]])](
-      ("@id", "id", Some(DocumentId.getIdFromPath)),
-      ("@type", "type", None)
-    ).foldLeft(Map[String, JsValue]()) {
-      case (map, (in: String, out: String, callback: Option[Function1[String, Option[String]]])) =>
-        callback match {
-          case Some(c) =>
-            val jsFieldValue = for {
-              instaceFieldJs <- instance.get(in)
-              instanceField  <- instaceFieldJs.asOpt[String]
-              r              <- c(instanceField)
-            } yield r
-            jsFieldValue match {
-              case Some(v) => map.updated(out, JsString(v))
-              case None    => map
-            }
-          case None =>
-            val jsFieldValue = for {
-              instaceFieldJs <- instance.get(in)
-              instanceField  <- instaceFieldJs.asOpt[List[String]]
-            } yield instanceField
-            jsFieldValue match {
-              case Some(i) => map.updated(out, Json.toJson(i))
-              case None    => map
-            }
-        }
-    }
-    val normalizedFields = instance
-      .filter(value => value._1 != "@id" && value._1 != "@type")
-      .map {
-        case (fieldName, field) => {
-          fieldsInfo.get(fieldName) match {
-            case Some(fieldInfo) =>
-              (fieldName, normalizeField(field.as[Map[String, JsValue]], fieldInfo.as[Map[String, JsValue]]))
-            case None => (fieldName, normalizeField(field.as[Map[String, JsValue]], Map[String, JsValue]()))
+  def normalizeFieldValue(value: JsValue, fieldInfo: Map[String, JsValue]): JsValue =
+    fieldInfo.get("link") match {
+      case Some(link) =>
+        if (link.as[Boolean]) {
+          value.asOpt[List[Map[String, JsValue]]] match {
+            case Some(valueArray) => Json.toJson(normalizeIdOfArray(valueArray))
+            case None =>
+              value.asOpt[Map[String, JsValue]] match {
+                case Some(valueObj) => Json.toJson(normalizeIdOfField(valueObj))
+                case None           => value
+              }
           }
+        } else {
+          value
         }
+      case None => value
+    }
+
+  def normalizeInstance(instance: JsObject, fieldsInfo: Map[String, Map[String, JsValue]]): Map[String, JsValue] = {
+
+    val normalizedInstance =
+      List[(String, String, Option[Function1[String, Option[String]]])]( // TODO: pass the casting type
+        ("@id", "id", Some(DocumentId.getIdFromPath)),
+        ("@type", "type", None)
+      ).foldLeft(Map[String, JsValue]()) {
+        case (map, (in: String, out: String, callback: Option[Function1[String, Option[String]]])) =>
+          callback match {
+            case Some(c) =>
+              val jsFieldValue = for {
+                instaceField <- (instance \ in).asOpt[String]
+                r            <- c(instaceField)
+              } yield r
+              jsFieldValue match {
+                case Some(v) => map.updated(out, JsString(v))
+                case None    => map
+              }
+            case None =>
+              val jsFieldValue = for {
+                instanceField <- (instance \ in).asOpt[List[String]]
+              } yield instanceField
+              jsFieldValue match {
+                case Some(i) => map.updated(out, Json.toJson(i))
+                case None    => map
+              }
+          }
       }
+    val normalizedFields = fieldsInfo.foldLeft(Map[String, Map[String, JsValue]]()) {
+      case (map, (fieldName, fieldInfo)) =>
+        (instance \ fieldName).asOpt[JsValue] match {
+          case Some(value) => map.updated(fieldName, fieldInfo.updated("value", normalizeFieldValue(value, fieldInfo)))
+          case None        => map
+        }
+    }
     normalizedInstance.updated("fields", Json.toJson(normalizedFields))
   }
 
-  def normalizeInstancesData(data: JsValue, fieldsInfoJs: JsValue): List[Map[String, JsValue]] = {
-    val fieldsInfo = (fieldsInfoJs \ "data").as[List[JsObject]].foldLeft(Map[String, JsObject]()) {
-      case (map, js) =>
-        map.updated((js \ s"${EditorConstants.EDITORNAMESPACE}describedField").as[String], js)
+  /*
+  {
+    "http://schema.org/name": {
+        type: "InputText",
+        label: "Name"
+    },
+    "http://schema.org/description": {
+        type: "InputText",
+        label: "Description",
+        markdown: true,
+        link: false
+    },
+    "http://schema.org/contributor": {
+        type: "DropDown",
+        label: "Contributors",
+        markdown: true,
+        link: true
     }
-    (data \ "data").as[List[Map[String, JsValue]]].map { instance =>
-      normalizeInstance(instance, fieldsInfo)
+  }
+   */
+  def getReconciledFields(
+    types: List[String],
+    fieldsInfo: Map[String, Map[String, Map[String, JsValue]]]
+  ): Map[String, Map[String, JsValue]] =
+    types.foldLeft(Map[String, Map[String, JsValue]]()) {
+      case (reconciledFieldsInfo, typeValue) => {
+        fieldsInfo.get(typeValue) match {
+          case Some(fieldsInfoRes) =>
+            fieldsInfoRes.foldLeft(reconciledFieldsInfo) {
+              case (map, (k, v)) => map.updated(k, v)
+            }
+          case None => reconciledFieldsInfo
+        }
+      }
+    }
+
+  /*
+  {
+    "http://schema.org/Dataset": {
+      "http://schema.org/name": {
+          type: "InputText",
+          label: "Name"
+      },
+      "http://schema.org/description": {
+          type: "InputText",
+          label: "Description",
+          markdown: true,
+          link: false
+      },
+      "http://schema.org/contributor": {
+          type: "DropDown",
+          label: "Contributors",
+          markdown: true,
+          link: true
+      }
+    }
+  }
+   */
+  def getFieldsInfoMapByType(fieldsInfo: List[JsObject]): Map[String, Map[String, Map[String, JsValue]]] =
+    fieldsInfo.foldLeft(Map[String, Map[String, Map[String, JsValue]]]()) {
+      case (map, typeInfo) =>
+        val res = for {
+          typeOpt   <- (typeInfo \ "type").asOpt[String]
+          fieldsOpt <- (typeInfo \ "fields").asOpt[List[Map[String, JsValue]]]
+        } yield (typeOpt, fieldsOpt)
+        res match {
+          case Some((typeValue, fieldsJs)) => {
+            val fields = fieldsJs.foldLeft(Map[String, Map[String, JsValue]]()) {
+              case (fieldsMap, field) =>
+                field.get("fieldType") match {
+                  case Some(fieldType) =>
+                    fieldsMap.updated(fieldType.as[String], generateProperties(field))
+                  case _ => fieldsMap
+                }
+            }
+            map.updated(typeValue, fields)
+          }
+          case _ => map
+        }
+    }
+
+  def tranformCanBe(canBe: List[String]): Boolean =
+    if (canBe.nonEmpty) {
+      true
+    } else {
+      false
+    }
+
+  def generateProperties(field: Map[String, JsValue]): Map[String, JsValue] =
+    List[(String, String, Option[Function1[List[String], Boolean]])](
+      ("canBe", "link", Some(tranformCanBe)),
+      ("widgetType", "type", None),
+      ("markdown", "markdown", None),
+      ("label", "label", None)
+    ).foldLeft(Map[String, JsValue]()) {
+      case (map, (in: String, out: String, callback: Option[Function1[List[String], Boolean]])) =>
+        callback match {
+          case Some(c) =>
+            field.get(in) match {
+              case Some(v) => map.updated(out, Json.toJson(c(v.as[List[String]])))
+              case None    => map
+            }
+          case None =>
+            field.get(in) match {
+              case Some(res) => map.updated(out, res)
+              case None      => map
+            }
+        }
+    }
+
+  def normalizeInstancesData(data: JsValue, fieldsInfoJs: JsValue): List[Map[String, JsValue]] = {
+    val fieldsInfo = getFieldsInfoMapByType((fieldsInfoJs \ "data").as[List[JsObject]])
+    (data \ "data").as[List[JsObject]].foldLeft(List[Map[String, JsValue]]()) {
+      case (list, instance) =>
+        (instance \ "@type").asOpt[List[String]] match {
+          case Some(instanceTypes) =>
+            val reconciledFields = getReconciledFields(instanceTypes, fieldsInfo)
+            list :+ normalizeInstance(instance, reconciledFields)
+          case _ => list
+        }
     }
   }
 
@@ -369,20 +450,28 @@ class EditorController @Inject()(
       val result = listOfIds match {
         case Some(ids) =>
           if (allFields) {
-            for {
-              instancesResult <- editorService.retrieveInstances(ids, request.userToken, databaseScope, metadata)
-              fieldsInfoResult <- editorService.retrieveTypes(
-                s"${EditorConstants.EDITORNAMESPACE}fieldInfo",
-                request.userToken,
-                metadata
-              )
-            } yield {
-              (instancesResult, fieldsInfoResult) match {
-                case (Right(instances), Right(fieldsInfo)) =>
-                  Ok(Json.toJson(EditorResponseObject(Json.toJson(normalizeInstancesData(instances, fieldsInfo)))))
-                case _ => InternalServerError("Something went wrong! Please try again!")
+            editorService
+              .retrieveInstances(ids, request.userToken, databaseScope, metadata)
+              .flatMap {
+                case Right(instancesResult) =>
+                  val typesToRetrieve = (instancesResult \ "data")
+                    .as[JsArray]
+                    .value
+                    .toList
+                    .flatMap(instance => (instance \ "@type").as[List[String]])
+                  editorService
+                    .retrieveTypesList(typesToRetrieve.distinct, request.userToken, true)
+                    .map {
+                      case Right(typesWithFields) =>
+                        Ok(
+                          Json.toJson(
+                            EditorResponseObject(Json.toJson(normalizeInstancesData(instancesResult, typesWithFields)))
+                          )
+                        )
+                      case _ => InternalServerError("Something went wrong! Please try again!")
+                    }
+                case _ => Task.pure(InternalServerError("Something went wrong! Please try again!"))
               }
-            }
           } else {
             editorService
               .retrieveInstances(ids, request.userToken, databaseScope, metadata)

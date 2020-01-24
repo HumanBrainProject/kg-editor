6c4351f51cabec54298951e61f6145cbab6ef2c8
David Kunzmann <davidkunzmann@gmail.com>
2020-01-24 09:18:00 +0100

    Clean up

--- service/build.sbt
+++ service/build.sbt
@@ -1,3 +1,18 @@
+/*
+ *   Copyright (c) 2018, EPFL/Human Brain Project PCO
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
 name := """kg-service"""
 organization := "eu.humanbrainproject"
 
@@ -15,7 +30,7 @@ lazy val editor = (project in file("modules/editor"))
   .dependsOn(common, auth)
 
 lazy val kg_service = (project in file("."))
-  .enablePlugins(PlayScala, DockerPlugin)
+  .enablePlugins(PlayScala)
   .aggregate(common, auth, editor)
   .dependsOn(common, auth, editor)
 
@@ -25,4 +40,3 @@ sources in (Compile, doc) := Seq.empty
 
 publishArtifact in (Compile, packageDoc) := false
 
-dockerBaseImage := "adoptopenjdk/openjdk11:jre-11.0.5_10-alpine"
--- service/project/Common.scala
+++ service/project/Common.scala
@@ -1,3 +1,18 @@
+/*
+ *   Copyright (c) 2019, EPFL/Human Brain Project PCO
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
 import sbt._
 import Keys._
 import play.sbt.PlayImport._
@@ -20,21 +35,20 @@ object Common {
     "-Ypartial-unification"
   )
 
+  val baseDependencies = Seq(
+    "de.leanovate.play-mockws" %% "play-mockws" % "2.6.2" % Test,
+    "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2" % Test,
+    "org.mockito" % "mockito-core" % "2.19.0" % Test,
+    "org.typelevel" %% "cats-core" % "1.4.0",
+    "io.monix" %% "monix" % "3.0.0-RC2"
+  )
+
   val settings: Seq[Setting[_]] = Seq(
     organization := "eu.humanbrainproject",
     version := "1.0.0-SNAPSHOT",
     scalaVersion := "2.12.7",
     libraryDependencies ++= baseDependencies,
     scalacOptions ++= scalacOptionsAll
-  )
-
-  val baseDependencies = Seq(
-    "de.leanovate.play-mockws" %% "play-mockws" % "2.6.2" % Test,
-    "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2" % Test,
-    "org.mockito" % "mockito-core" % "2.19.0" % Test,
-    "com.github.stijndehaes" %% "play-prometheus-filters" % "0.3.2",
-    "org.typelevel" %% "cats-core" % "1.4.0",
-    "io.monix" %% "monix" % "3.0.0-RC2"
   )
 
   val playDependencies = Seq(

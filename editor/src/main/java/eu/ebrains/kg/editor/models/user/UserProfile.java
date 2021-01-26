263173ddfba8375d3f7d0d54cb37cb2b12693d87
Oliver Schmid <oliver.schmid@epfl.ch>
2021-01-26 16:05:13 +0100

    fix user profile

--- editor/src/main/java/eu/ebrains/kg/editor/models/user/UserProfile.java
+++ editor/src/main/java/eu/ebrains/kg/editor/models/user/UserProfile.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import eu.ebrains.kg.editor.constants.SchemaFieldsConstants;
 import eu.ebrains.kg.editor.models.commons.UserSummary;
+import org.springframework.util.CollectionUtils;
 
 import java.util.List;
 
@@ -11,13 +12,13 @@ public class UserProfile extends UserSummary {
 
     @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
     public UserProfile(
-            @JsonProperty("@id") String kgId,
+            @JsonProperty(SchemaFieldsConstants.IDENTIFIER) List<String> kgId,
             @JsonProperty(SchemaFieldsConstants.ALTERNATENAME) String kgUserName,
             @JsonProperty(SchemaFieldsConstants.NAME) String kgName,
             @JsonProperty(SchemaFieldsConstants.GIVEN_NAME) String kgGivenName,
             @JsonProperty(SchemaFieldsConstants.FAMILY_NAME) String kgFamilyName,
             @JsonProperty(SchemaFieldsConstants.EMAIL) String kgEmail){
-        super(kgId, kgUserName, kgName);
+        super(!CollectionUtils.isEmpty(kgId) ? kgId.get(0) : null, kgUserName, kgName);
         this.givenName = kgGivenName;
         this.familyName = kgFamilyName;
         this.email = kgEmail;

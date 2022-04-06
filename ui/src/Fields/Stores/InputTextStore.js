94362232199f60eb44c3c36ecdc107b47af7b636
gdenervaud <gilles.denervaud@gmail.com>
2022-04-06 13:42:00 +0200

    Add warning if a text area has trailing new lines

--- ui/src/Fields/Stores/InputTextStore.js
+++ ui/src/Fields/Stores/InputTextStore.js
@@ -26,7 +26,7 @@ import { observable, action, computed, toJS, makeObservable } from "mobx";
 import FieldStore from "./FieldStore";
 
 const DEFAULT_REGEX = {
-  regex: /^(?! )(.|\n)*(?<! )$/,
+  regex: /^(?!\s)(.|\n)*(?<!\s)$/,
   errorMessage: "leading/trailling spaces are not allowed"
 };
 

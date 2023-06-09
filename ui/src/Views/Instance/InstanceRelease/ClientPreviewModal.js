631d6b6b6864adef9c21be1217c7c1a0a2eeab0e
Ioannis Tsanaktsidis <tsanakts.10@gmail.com>
2023-06-09 14:27:16 +0200

    remove origin

--- ui/src/Views/Instance/InstanceRelease/ClientPreviewModal.js
+++ ui/src/Views/Instance/InstanceRelease/ClientPreviewModal.js
@@ -82,7 +82,6 @@ const ClientPreviewModal = observer(({ store, show, handleClose }) => {
           <Iframe url={url}
             width="100%"
             height="100%"
-            allow="same-origin"
             id={store.topInstanceId}
             className={classes.frame}
             display="initial"
